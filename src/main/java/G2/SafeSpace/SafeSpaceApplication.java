206e73baffd1a4690a963064df726471bc4cb24b
JoelPalu <kirill.saveliev@metropolia.fi>
2024-08-26 16:22:45 +0300

    Initial commit

---
+++ src/main/java/G2/SafeSpace/SafeSpaceApplication.java
@@ -0,0 +1,13 @@
+package G2.SafeSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/G2/SafeSpace/SafeSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package G2.SafeSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
