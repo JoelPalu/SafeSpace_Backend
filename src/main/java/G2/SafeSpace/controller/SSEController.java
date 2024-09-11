e6c4c9f52ee4cc9248d60a137478b1928d6a98d8
mirohi <m_hintikka@hotmail.com>
2024-09-11 15:40:04 +0300

    Add SSE endpoint

---
+++ src/main/java/G2/SafeSpace/controller/SSEController.java
@@ -0,0 +1,30 @@
+package G2.SafeSpace.controller;
+
+import G2.SafeSpace.entity.Post;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/posts")
+public class SSEController {
+
+    @GetMapping("/")
+        public Flux<ServerSentEvent<String>> getEvents () throws InterruptedException, IOException {
+        ArrayList<Post> posts = new ArrayList<>();
+
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(seq -> ServerSentEvent.<String>builder()
+                        .id(String.valueOf(seq))
+                        .event("message")
+                        .data("Event #" + seq)
+                        .build());
+    }
+}
+
